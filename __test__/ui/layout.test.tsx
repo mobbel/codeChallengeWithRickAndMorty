a466c7928cc3d2c69b7a7589724f47ae06bb4125
Stephan Sperling <sperling.stephan@googlemail.com>
2022-09-12 22:34:49 +0200

    added testenvironment

---
+++ __test__/ui/layout.test.tsx
@@ -0,0 +1,16 @@
+import { SessionProvider } from 'next-auth/react';
+import renderer from 'react-test-renderer';
+import Layout from "../../src/ui/layout";
+
+describe('>>>Testing layout',() => {
+  test('+++ Snapshot of Layout', () => {
+    window.fetch = jest.fn();
+    const component: any = renderer.create(
+      <SessionProvider session={null}>
+        <Layout><div>Test</div></Layout>
+      </SessionProvider>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+})
