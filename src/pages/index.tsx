d8c140e358ce46c20521faf844d3f6dc7f1c7112
Stephan Sperling <sperling.stephan@googlemail.com>
2022-09-15 15:42:07 +0200

    added image to startpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from "next";
+import IndexAboveFold from "../ui/Index/IndexAboveFold";
 
 const Home: NextPage = () => {
   return (
     <>
+      <IndexAboveFold />
+      <h1 className="text-center font-medium leading-6 text-gray-900">Das könnte eure Startseite sein.</h1>
     </>
   );
 };
